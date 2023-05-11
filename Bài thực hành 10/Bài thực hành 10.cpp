8ff9d2fde7a12c70539a402719460f655cdfca9e
Trà My <133077624+tramy2409@users.noreply.github.com>
2023-05-11 15:41:26 +0700

    Add files via upload

---
+++ Bài thực hành 10/Bài thực hành 10.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<string>  
+using namespace std;
+
+void longest(string a, string b){  // Hàm tìm chuỗi chung dài nhất và in ra màn hình
+    int n = a.size();  // n chiều dài chuỗi a, m chiều chuỗi xâu b
+    int m = b.size();
+    int max_Size;     // Biến lưu độ dài con chung lớn nhất
+    string temp = "";  // Biến lưu con chung dùng khi truy vết
+    int L[n+1][m+1];   // Khai báo mảng lưu kết quả:  n+1 hàng, m+1 cột
+    
+    for(int i=0; i<=n; i++)   // Gán cột đầu tiên bằng 0
+        L[i][0] = 0;
+    for(int j=0; j<=m; j++)   // Gán hàng đầu tiên = 0
+        L[0][j] = 0;
+        
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=m; j++){
+            if(a[i-1] == b[j-1]){  // Nếu có phần tử bằng nhau
+                L[i][j] = L[i-1][j-1] + 1;   // Áp dụng công thức
+            }
+            else{   // Trường hợp a[i-1] khác b[j-1]
+                if(L[i-1][j] >= L[i][j-1])     // Tìm max giữa L[i-1][j] và L[i][j-1]
+                    L[i][j] = L[i-1][j];
+                else
+                    L[i][j] = L[i][j-1];
+            }
+        }
+    }
+    
+    max_Size = L[n][m];  // Tìm được độ dài chuỗi chung lớn nhất
+    int i = n; 
+    int j = m;
+    while(L[i][j] != 0){ // Điều kiện dừng
+        if(a[i-1] == b[j-1]){  // Nếu bằng nhau
+            temp += a[i-1];   // Cộng a[i-1] vào chuỗi chung
+            i--;
+            j--;
+        }
+        else{  // Nếu khác nhau
+            if(L[i-1][j] >= L[i][j-1]) // So sánh
+                i--;
+            else
+                j--;
+        }
+    }
+    
+    cout<<"\nDo dai xau lon nhat: "<<max_Size;  // In ra độ dài chuỗi chung lớn nhất
+    cout<<"\nXau con: ";
+    for(int t = max_Size-1 ; t>=0; t--)  // In ngược từ cuối về đầu để chuỗi chung đúng thứ tự
+        cout<<temp[t];
+}
+int main(){
+    string a, b;
+    cout<<"Nhap chuoi a: "; 
+    cin>>a;
+    cout<<"Nhap chuoi b: "; 
+    cin>>b;
+    longest(a,b);
+    return 0;    
+}
